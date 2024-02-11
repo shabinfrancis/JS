d66f36b237a03eda61dc094e867c9d7b82987b6e
shabinfrancis <142815191+shabinfrancis@users.noreply.github.com>
2024-02-11 18:15:07 +0530

    JSpart13 files added

---
+++ JSpart13/app.js
@@ -0,0 +1,35 @@
+let url = "http://universities.hipolabs.com/search?state-province=";
+let btn = document.querySelector("button");
+ 
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    // await getColleges(country);
+
+    let colArr = await getColleges(country);
+    show(colArr);
+});
+
+function show(colArr){
+
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of colArr){
+
+        console.log(col.name);
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
+
+async function getColleges(country) {
+
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch(e){
+        console.log("error: ",e);
+        return []; 
+    }
+} 
